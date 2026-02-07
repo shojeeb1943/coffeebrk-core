4e817d4f71eab8effbdd7ae35ac11f2d46bd636c
Md Reyajul Islam <shojeeb1943@gmail.com>
2026-02-07 21:06:56 +0600

    Update stories nav styles and bump version
    Refactor coffeebrk-stories CSS to simplify navigation button visuals: remove box-shadows and scaling, change hover background to black with white SVG stroke, set stroke to black by default, shorten transition timing, and reset prev/next margins to 0. Also bump the enqueued stylesheet version from 1.9.3 to 1.9.4 to ensure browsers pick up the updated CSS.

--- inc/class-coffeebrk-elementor-widgets.php
+++ inc/class-coffeebrk-elementor-widgets.php
@@ -96,7 +96,7 @@ add_action( 'elementor/frontend/after_enqueue_styles', function() {
         'coffeebrk-stories',
         COFFEEBRK_CORE_URL . 'assets/css/coffeebrk-stories.css',
         [],
-        '1.9.3'
+        '1.9.4'
     );
 } );
 
